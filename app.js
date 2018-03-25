78e9d8bf8d03609f88a07a8841d2a2a4c36ef1f7
li_jerry <lijerrymagic@gmail.com>
2018-03-25 17:52:09 -0400

    add pics

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/index', (req, res) => res.redirect('/index.html'))
 app.get('/about', (req, res) => res.redirect('/about.html'))
 app.get('/contact', (req, res) => res.redirect('/contact.html'))
 app.get('/activity', (req, res) => res.redirect('/activity.html'))
+app.get('/media', (req, res) => res.redirect('/media.html'))
 // app.get('/', function (req, res, next) {
 //     return res.redirect('/index.html');
 // });
@@ -18,9 +19,9 @@ app.use(express.static('frontend'));
 
 var http = require('http');
 
-http.createServer(app).listen(process.env.PORT, function () {
-    console.log('HTTP on port');
-});
-// http.createServer(app).listen(3000, function () {
-//     console.log('HTTP on port 3000');
+// http.createServer(app).listen(process.env.PORT, function () {
+//     console.log('HTTP on port');
 // });
+http.createServer(app).listen(3000, function () {
+    console.log('HTTP on port 3000');
+});
